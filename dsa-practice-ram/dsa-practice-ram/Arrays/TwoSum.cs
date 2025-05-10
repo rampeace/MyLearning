c8af0ea58e26aee2de6546d2eb5138c3c752bd01
Ram Kumar R <ram.peace@gmail.com>
2025-05-10 09:31:24 +0530

    Added a comment for two sum

--- dsa-practice-ram/dsa-practice-ram/Arrays/TwoSum.cs
+++ dsa-practice-ram/dsa-practice-ram/Arrays/TwoSum.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace dsa_practice_ram.Arrays
         public void FindTwoSum()
         {
             int[] array = { 2, 6, 4, 6, 8, 5, 8, 1, 1, 10 };
+            // sorted: { 1, 1, 2, 4, 5, 6, 8, 8, 10 }
             int target = 14;
             var pairs = new List<(int x, int y)>();
             var seen = new HashSet<int>();
