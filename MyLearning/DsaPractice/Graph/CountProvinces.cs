63d02fe97cea5cf0aee699e78c52eb49eae95b8b
Ram Kumar R <ram.peace@gmail.com>
2025-05-28 09:59:06 +0530

    Update CountProvinces.cs

--- MyLearning/DsaPractice/Graph/CountProvinces.cs
+++ MyLearning/DsaPractice/Graph/CountProvinces.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +20,7 @@ namespace DsaPractice.Graph
          *  
          *      0  1  2
          *     ---------
-             0 [1, 0, 1],
+             0 [1, 1, 0],
              1 [1, 1, 0],
              2 [0, 0, 1]
            
