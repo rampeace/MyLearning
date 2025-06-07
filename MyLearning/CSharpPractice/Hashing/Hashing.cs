7856674a3d7eaca83f4be3bd229492b25ed5fdc0
Ram Kumar R <ram.peace@gmail.com>
2025-06-07 09:39:40 +0530

    Update Hashing.cs

--- MyLearning/CSharpPractice/Hashing/Hashing.cs
+++ MyLearning/CSharpPractice/Hashing/Hashing.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +16,12 @@ namespace CSharpPractice.Hashing
             public override int GetHashCode()
             {
                 // Use hash code combination pattern
+                /* The goal:
+
+                -Every bit of your 32-bit hash should be influenced by every bit of your key.
+                -That way, the hash code is well distributed—meaning keys are spread across the dictionary’s slots with minimal clustering.
+                -Result: Faster lookups, inserts, and removals—because you have fewer collisions.
+                */
                 int hash = 17; // initial seed
                 hash = hash * 23 + (Name?.GetHashCode() ?? 0); // 23: Prime multiplier to spread hash values
                 hash = hash * 23 + Age.GetHashCode();
