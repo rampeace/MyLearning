f8728a1f36c4971f76c880a0f751b8f4baec0d7c
Ram Kumar R <ram.peace@gmail.com>
2026-05-07 10:15:40 +0530

    Added Iterator Design pattern sample class

---
+++ MyLearning/CSharpPractice/DesignPatterns/Iterator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.DesignPatterns
+{
+  public class BookCollection : IEnumerable
+  {
+      private string[] _books =
+      {
+          "Clean Code",
+          "Design Patterns",
+          "CLR via C#"
+      };
+  
+      public IEnumerator GetEnumerator()
+      {
+          return new BookIterator(_books);
+      }
+  }
+  
+  public class BookIterator : IEnumerator
+  {
+      private string[] _books;
+      private int _position = -1;
+  
+      public BookIterator(string[] books)
+      {
+          _books = books;
+      }
+  
+      public bool MoveNext()
+      {
+          _position++;
+          return _position < _books.Length;
+      }
+  
+      public void Reset()
+      {
+          _position = -1;
+      }
+  
+      public object Current
+      {
+          get
+          {
+              return _books[_position];
+          }
+      }
+  }
+
+// Modern C# Version
+public class BookCollection
+{
+    private List<string> _books = new()
+    {
+        "Clean Code",
+        "Design Patterns",
+        "CLR via C#"
+    };
+
+    public IEnumerable<string> GetBooks()
+    {
+        foreach (var book in _books)
+        {
+            yield return book;
+        }
+    }
+  }
+}
